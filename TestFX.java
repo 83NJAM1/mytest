c73092eea15461dd6365c8edcda627faa7f99b24
ben <ben@Debian>
2022-09-16 18:01:13 +0200

    my commit

--- TestFX.java
+++ TestFX.java
@@ -2,10 +2,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class TestFX extends Application{
+	
 	public void start(Stage stage) {
 		stage.show();
 	}
 	public static void main(String[] args) {
+		System.out.println("start");
 		launch();
 	}
 }
